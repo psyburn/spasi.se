2fb8c428b2bce47bbd6963afc9dc96e7c6f5c40e
Ivan Lučin <lucin.ivan@gmail.com>
2013-10-19 19:28:30 +0200

    stuff

--- app/scripts/views/details/main.js
+++ app/scripts/views/details/main.js
@@ -12,13 +12,13 @@ define([
     events: {},
 
     render: function() {
-      this.model.set('phone', '123891283123');
-      this.model.set('address', 'Slavka Krautzeka 40 Rijeka');
-      this.model.set('description', 'Blaksdj aklsd alksdm aklsdm aksdm aklsdm as,d amskld asmnd ');
-      this.model.set('working_hours', {
-        "start": [0, 0, 0, 0, 0, 0, 0],
-        "end": [24, 24, 24, 24, 24, 24, 24]
-      });
+      // this.model.set('phone', '123891283123');
+      // this.model.set('address', 'Slavka Krautzeka 40 Rijeka');
+      // this.model.set('description', 'Blaksdj aklsd alksdm aklsdm aksdm aklsdm as,d amskld asmnd ');
+      // this.model.set('working_hours', {
+      //   "start": [0, 0, 0, 0, 0, 0, 0],
+      //   "end": [24, 24, 24, 24, 24, 24, 24]
+      // });
       this.$el.html(this.template({
         item: this.model.toJSON()
       }));
