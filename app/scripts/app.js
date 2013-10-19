d7a6d56363917c29d856f499aec80927f178e7e9
Ivan Lučin <lucin.ivan@gmail.com>
2013-10-19 11:08:28 +0200

    Added maps view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,33 @@
 /*global define */
-define([], function () {
+define([], function() {
   'use strict';
 
   var app = {
-    root: '/'
+    root: '/',
+
+    gmaps: {
+      url: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyANYgRiD8Ra92P08fC0rm3v_TPLkRitLQw&sensor=true'
+    },
+
+    loadGmaps: function(context, callback) {
+      if (!this.gmaps.loaded) {
+        this.gmaps.loadContext = context;
+        this.gmaps.loadCallback = callback;
+
+        $('<script>')
+          .attr('type', 'text/javascript')
+          .attr('src', this.gmaps.url + '&libraries=places&callback=app.onGmapsLoad')
+          .appendTo('head');
+
+        this.gmaps.loaded = true;
+      }
+    },
+
+    onGmapsLoad: function() {
+      if (this.gmaps.loadCallback && this.gmaps.loadContext) {
+        this.gmaps.loadCallback.call(this.gmaps.loadContext);
+      }
+    }
   };
 
   return app;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,6 +26,7 @@ require(['app',
   // use app here
 
   console.log(app);
+  window.app = app;
 
   new MainRouter();
 
