069c7e33e9e46857d3ebfa49e403f4310febc5a3
Darko Kukovec <darko.kukovec@infinum.hr>
2013-10-19 12:26:42 +0200

    locations loading (wip)

--- app/scripts/collections/locations.js
+++ app/scripts/collections/locations.js
@@ -5,8 +5,38 @@ define([
     'use strict';
 
     var LocationsCollection = Backbone.Collection.extend({
-      _parse_class_name: "Location",
-      model: Model
+      _parse_class_name: 'Location',
+      model: Model,
+
+      load: function(options, callback) {
+        // Categories
+        // Location
+        // Work hours
+        var query = {
+          'category': {'$in': options.categories},
+          'location': {
+            '$within': {
+              '$box': [
+                {
+                  '__type': 'GeoPoint',
+                  'latitude': options.location.sw.lat,
+                  'longitude': options.location.sw.lon
+                },
+                {
+                  '__type': 'GeoPoint',
+                  'latitude': options.location.ne.lat,
+                  'longitude': options.location.ne.lon
+                }
+              ]
+            }
+          }
+        };
+        // TOO: Work hours
+        this.fetch({
+          query: query,
+          success: callback
+        });
+      }
     });
 
     return LocationsCollection;
