846cabf321c4aa6b649b8b0b6ed643410778972c
psyburn <psykill@gmail.com>
2013-10-19 17:56:06 +0200

    added toucher.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,8 @@ require.config({
     jquery: '../bower_components/jquery/jquery',
     backbone: '../bower_components/backbone/backbone',
     underscore: '../bower_components/lodash/dist/lodash',
+
+    toucher: 'plugins/toucher',
   },
   shim: {
     backbone: {
@@ -28,14 +30,16 @@ require([
   'collections/places',
 
   //plugins
+  'toucher',
   'backbone-parse',
   'Points',
-  'scooch'
+  'scooch',
 ], function(
     app,
     MainRouter,
     LocationsCollection,
-    PlacesCollection
+    PlacesCollection,
+    Toucher
   ) {
     'use strict';
     // use app here
@@ -69,7 +73,7 @@ require([
       },
       workingHours: false
     };
-
+    Toucher.bind('.touchable');
     Backbone.history.start({
       pushState: false,
       root: app.root
