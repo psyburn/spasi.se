0ab5044c733ed2f27cc2aa9f23a97ec65ee1b244
Ivan Lučin <lucin.ivan@gmail.com>
2013-10-19 18:33:46 +0200

    details

--- app/scripts/views/details/main.js
+++ app/scripts/views/details/main.js
@@ -12,10 +12,19 @@ define([
     events: {},
 
     render: function() {
-      this.$el.html(this.template(this.model.toJSON()));
+      this.model.set('phone', '123891283123');
+      this.model.set('address', 'Slavka Krautzeka 40 Rijeka');
+      this.model.set('description', 'Blaksdj aklsd alksdm aklsdm aksdm aklsdm as,d amskld asmnd ');
+      this.model.set('working_hours', {
+        "start": [0, 0, 0, 0, 0, 0, 0],
+        "end": [24, 24, 24, 24, 24, 24, 24]
+      });
+      this.$el.html(this.template({
+        item: this.model.toJSON()
+      }));
       this.$('.gallery').scooch();
       return this;
-    },
+    }
 
   });
 
